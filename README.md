3a85e54197605b91ee21ea96050e0dc02f199fb2
timfame-codespace <timagrib1111@gmail.com>
2023-06-25 00:42:15 +0200

    Init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Optimism decent minter
+
+ - [Mint Optimism RetroPGF 3](https://optimism.decent.xyz/)
+
+### Settings
+`files/wallets.txt` - Wallets with private keys \
+`files/proxies.txt` - Corresponding proxies for wallets \
+`config.py` - Custom settings \
+`vars.py` - Contracts info 
+
+### Run
+
+Installing all dependencies: \
+`pip3 install -r requirements.txt`
+
+Run script: \
+`python3 main.py`
+
+### Results
+
+`results/` - Folder with results by datetime of run \
+`logs/` - Folder with logs by datetime of run
+
+### Donate :)
+
+TRC-20 - `TX7yeJVHwhNsNy4ksF1pFRFnunF1aFRmet` \
+ERC-20 - `0x5aa3c82045f944f5afa477d3a1d0be3c96196319`
---
+++ utils.py
@@ -0,0 +1,44 @@
+import requests
+import ua_generator
+from web3 import Web3
+from config import RPCs
+from vars import ERC20_TOKEN_ABI, CHAIN_NAMES
+
+
+def get_coin_price(coin, currency):
+    resp = requests.get(
+        f'https://api.coingecko.com/api/v3/coins/{coin}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false')
+    return float(resp.json()['market_data']['current_price'][currency])
+
+
+def get_w3(chain, proxy=None):
+    req_args = {} if proxy is None or proxy == '' else {
+        'proxies': {'https': proxy, 'http': proxy},
+    }
+    return Web3(Web3.HTTPProvider(RPCs[chain], request_kwargs=req_args))
+
+
+def get_chain(w3):
+    return CHAIN_NAMES[w3.eth.chain_id]
+
+
+def send_tx(w3, private_key, tx, verify_func, action):
+    estimate = w3.eth.estimate_gas(tx)
+    tx['gas'] = estimate
+
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    verify_func(get_chain(w3), tx_hash, action=action)
+
+    return tx_hash
+
+
+def build_and_send_tx(w3, address, private_key, func, value, verify_func, action):
+    tx = func.build_transaction({
+        'from': address,
+        'nonce': w3.eth.get_transaction_count(address),
+        'gasPrice': w3.eth.gas_price,
+        'value': value,
+    })
+
+    return send_tx(w3, private_key, tx, verify_func, action)
